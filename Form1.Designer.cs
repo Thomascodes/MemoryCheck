97d3188a55a220486fe38d9f8a6049af04fe93f2
Thomascodes <pillowtech24@gmail.com>
2020-03-04 15:39:23 -0500

    initial commit
    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Fr4meC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.button1.Location = new System.Drawing.Point(12, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MemoryCheck";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label_title);
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 101);
+            this.panel1.TabIndex = 1;
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Noto Sans Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.Location = new System.Drawing.Point(3, 11);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(152, 25);
+            this.label_title.TabIndex = 0;
+            this.label_title.Text = "MemoryCheck";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to MemoryCheck, the re-written version of Fr4meDate. This is an updater f" +
+    "or everything related to NewSeg. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(708, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BUILD NOTES: Please excuse the lack of content and the simple UI. An enormous UI-" +
+    "redesign is coming when more content is added to the project.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(423, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MemoryCheck Build 1.0.0.0 (\"Chipset\") - AN INTERNET CONNECTION IS REQUIRED!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Noto Sans Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(369, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "an updater for MemorySeg programs";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.NavajoWhite;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(12, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 41);
+            this.panel2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Noto Sans Cond", 10F);
+            this.label5.Location = new System.Drawing.Point(5, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(303, 19);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "one-stop hub for all new versions of MemorySeg works";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fr4meC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //creating a fixed size and disabling maximize as there's no reason to change the size of the program
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string to read back text of button, therefore naming the program in the MsgBox
+            string bTitle = this.button1.Text;
+
+           
+            {
+        {
+            {
+                       
+                WebRequest wr = WebRequest.Create(new Uri("https://tjloudon.me/ver/frm/version.txt"));
+
+                WebResponse ws = wr.GetResponse();
+                StreamReader sr = new StreamReader(ws.GetResponseStream());
+
+                        string currentver = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                string newver = sr.ReadToEnd();
+
+
+                if (currentver.Contains(newver))
+                {
+                            MessageBox.Show("Program " + bTitle + " is up to date at build " + newver + ".");
+                }
+                else
+                {
+                            TaskDialog dialog = new TaskDialog();
+                            dialog.InstructionText = "Out of Date!";
+                            dialog.Text = "Your build of " + bTitle + " is out of date! It is recommended that you update, but this is optional.";
+                            dialog.Icon = TaskDialogStandardIcon.Error;
+                            dialog.FooterText = "The newest build of " + bTitle + " is " + newver + ".";
+                            dialog.Show();
+
+                        }
+                
+            }
+        }
+    }
+
+}
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
